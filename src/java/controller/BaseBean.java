bd0206e0201207f009b4f62010424c16d6b97800
CanselYilmaz <>
2020-05-27 03:37:26 +0300

    Dosya işlemleri,Pagination,Tek kaydin okunmasi yapıldı ... FileValidation,BaseBean,FileServlet , upload dosyası  eklendi .. module klasörüne xhtml sayflar(form-table) eklendi.. 4 tane dosya (resim) eklendi CanselYılmaz,Gülistan Çolak,Betül Akmeşe  tarafından işlemler yapıldı

---
+++ src/java/controller/BaseBean.java
@@ -0,0 +1,70 @@
+
+package controller;
+
+
+abstract class BaseBean{
+    // arama tek  kaydin okunmasi 
+    
+    private String searchTerm;
+	
+	public void search() {
+		this.setPage(1);
+	}
+	
+	public void clearSearch() {
+		this.setSearchTerm(null);
+	}
+
+	public String getSearchTerm() {
+		return searchTerm;
+	}
+
+	public void setSearchTerm(String searchTerm) {
+		this.searchTerm = searchTerm;
+	}
+	
+/////////////////////////////////////////////////////
+        
+        //sayfalama 
+        
+    protected int page = 1;
+    protected  static  int pageSize = 3; 
+    protected int pageCount; 
+
+    public void next() {
+        if (this.page == this.getPageCount())// eğer son sayfaya eşitse 
+        {
+            this.page = 1;
+        } else {
+            this.page++;
+        }
+
+    }
+
+    public void previous() {
+        if (this.page == 1) {
+            this.page = this.getPageCount();
+        } else {
+            this.page--;
+        }
+    }
+    public int getPage() {
+        return page;
+    }
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+    abstract int getPageCount();
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+    public static void setPageSize(int pageSize) {
+        BaseBean.pageSize = pageSize;
+    }
+    
+    /////
+}
---
+++ src/java/controller/DocumentController.java
@@ -0,0 +1,136 @@
+package controller;
+
+import dao.DocumentDao;
+import entity.Document;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Named;
+import javax.servlet.http.Part;
+import java.lang.String;
+
+@Named
+@SessionScoped
+
+public class DocumentController extends BaseBean implements Serializable, Validator {
+
+    private Document document;
+    private List<Document> documentList;
+    private DocumentDao documentDao;
+
+    private Part doc;
+
+    private final String uploadTo = "C:\\Users\\tr\\Documents\\NetBeansProject\\EmlaKonutProje1\\upload\\";
+
+    public void updateForm(Document document) {
+        this.document = document;
+    }
+
+    public void upload() {
+        try {
+
+            InputStream input = doc.getInputStream();
+            File f;
+            f = new File(uploadTo + doc.getHeader(uploadTo));
+            Files.copy(input, f.toPath());
+
+            String[] tmp = f.getName().split("\\.");
+            String extension = tmp[tmp.length - 1];
+            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy.hh.mm.ss");
+            String a = sdf.format(new Date());
+
+            File newFile = new File("C:\\Users\\tr\\Documents\\NetBeansProject\\EmlaKonutProje1\\upload\\" + a + "." + extension);
+            f.renameTo(newFile);
+
+            document = this.getDocument();
+            document.setFilePath(f.getParent());
+            document.setFileName(a + "." + extension);
+            document.setFileType(doc.getContentType());
+
+            this.getDocumentDao().insert(document);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void clearUpload() {
+        this.setDoc(null);
+    }
+
+    public String getUploadTo() {
+        return uploadTo;
+    }
+
+    public Document getDocument() {
+        if (this.document == null) {
+            this.document = new Document();
+        }
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    public List<Document> getDocumentList() {
+        this.documentList = this.getDocumentDao().findAll(page, pageSize, this.getSearchTerm());
+        return documentList;
+    }
+
+    public void setDocumentList(List<Document> documentList) {
+        this.documentList = documentList;
+    }
+
+    public DocumentDao getDocumentDao() {
+        if (this.documentDao == null) {
+            this.documentDao = new DocumentDao();
+        }
+        return documentDao;
+    }
+
+    public void setDocumentDao(DocumentDao documentDao) {
+        this.documentDao = documentDao;
+    }
+
+    public Part getDoc() {
+        return doc;
+    }
+
+    public void setDoc(Part doc) {
+        this.doc = doc;
+    }
+
+    @Override
+    public int getPageCount() {
+        this.pageCount = (int) Math.ceil(this.getDocumentDao().count(this.getSearchTerm()) / (double) pageSize);
+        return pageCount;
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        Part partObject = (Part) value;
+        if (partObject != null) {
+            if (partObject.getSize() > 10000000) {
+                throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, " maksimum 10 megabyte", " maksimum 10 megabyte"));
+            }
+
+            if (!"image/jpeg".equals(partObject.getContentType())) {
+                throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "dosya turu sadece Jpg , jgp seçiniz lütfen", "dosya turu sadece Jpg"));
+
+            }
+        }
+
+    }
+}
